71409e163e199f5218d79b21ac63c5dd5e1ec239
Elgun <novruzluqyds20@gmail.com>
2023-04-26 06:30:15 +0400

    6:29 AM  -> 4/26/2023

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp1;
 using System;
+using System.Security.Cryptography.X509Certificates;
+
 class Program {
     static void Main(string[] args)
     {
-        talkingcat Tom = new talkingcat("Tom", 0, "Male", 100, 0, 0);
-        Tom.Game();
+        cat c1 = new cat("Mike", 12, "Male", 100, 120, 0);
+        cat c2 = new cat("Tyson", 11, "Male", 100, 140, 0);
+        cat c3 = new cat("Jane", 2, "Female", 100, 120, 0);
+
+        cat[] cats = new cat[3] { c1, c2, c3 };
+
+        dog d1 = new dog("Mike", 12, "Male", 100, 120, 0);
+        dog d2 = new dog("Tyson", 11, "Male", 100, 140, 0);
+        dog d3 = new dog("Jane", 2, "Female", 100, 120, 0);
+
+        dog[] dogs = new dog[3] { d1, d2, d3 };
+        petshop petshop = new petshop(dogs, cats);
+        petshop.remove();
+        Console.WriteLine(petshop.ToString());
     }
 }
--- ConsoleApp1/petshop.cs
+++ ConsoleApp1/petshop.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,16 +14,122 @@ namespace ConsoleApp1
         public dog[] Dogs { get; set; }
 
         // => Parametrized Constructor of our class
-        public petshop(dog[] Dogs , cat[] Cats) {
+        public petshop(dog[] Dogs, cat[] Cats)
+        {
             this.Cats = Cats;
             this.Dogs = Dogs;
         }
 
         // => Remove by nickname
-        public void remove() {
-            Console.WriteLine("[1] Remove from Cats by nickname");
-            Console.WriteLine("[2] Remove from Cats by nickname");
-            
+        public void remove()
+        {
+            ConsoleKeyInfo key;
+            string nickname;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("[1] Remove from Cats by nickname");
+                Console.WriteLine("[2] Remove from Dogs by nickname");
+                Console.Write("Choice here...");
+                key = Console.ReadKey();
+                if (key.Key == ConsoleKey.NumPad1)
+                {
+                    break;
+                }
+                else if (key.Key == ConsoleKey.NumPad2)
+                {
+                    break;
+                }
+            }
+            Console.Clear();
+            Console.Write("Enter nickname...");
+            bool exist = false;
+            nickname = Console.ReadLine();
+            if (key.Key == ConsoleKey.NumPad1)
+            {
+                foreach(cat c in Cats) {
+                    if (nickname == c.nickname) {
+                        exist = true;
+                    }
+                }
+                if (exist)
+                {
+                    cat[] NewCats = new cat[Cats.Length - 1];
+                    int counter = 0;
+                    for (int i = 0; i < Cats.Length; i++)
+                    {
+                        if (Cats[i].nickname != nickname)
+                        {
+                            NewCats[counter] = Cats[i];
+                            counter++;
+                        }
+                    }
+                    Cats = NewCats;
+                }
+                else {
+                    Console.Clear();
+                    Console.WriteLine("This cat does not exist !");
+                }
+            }
+            else if (key.Key == ConsoleKey.NumPad2)
+            {
+                foreach (dog d in Dogs)
+                {
+                    if (nickname == d.nickname)
+                    {
+                        exist = true;
+                    }
+                }
+                if (exist)
+                {
+                    dog[] NewDogs = new dog[Dogs.Length - 1];
+                    int counter = 0;
+                    for (int i = 0; i < Dogs.Length; i++)
+                    {
+                        if (Dogs[i].nickname != nickname)
+                        {
+                            NewDogs[counter] = Dogs[i];
+                            counter++;
+                        }
+                    }
+                    Dogs = NewDogs;
+
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("This dog does not exist !");
+                }
+            }
+        }
+
+        // => See result
+        public override string ToString() {
+            string? result = null;
+            while (true)
+            {
+                //Console.Clear();
+                Console.WriteLine("[1] -> Show Cats");
+                Console.WriteLine("[2] -> Show Dogs");
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.NumPad1)
+                {
+                    foreach (cat c in Cats)
+                    {
+                        result += c.ToString() + "\n";
+                    }
+                    break;
+                }
+                else if (key.Key == ConsoleKey.NumPad2)
+                {
+                    foreach (dog d in Dogs)
+                    {
+                        result += d.ToString() + "\n";
+                    }
+                    break;
+                }
+            }
+            return result ?? "+";
         }
     }
 }
